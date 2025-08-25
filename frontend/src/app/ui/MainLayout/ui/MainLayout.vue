e63e24ec1cbe93cd0fa26e14a24e1e297f57e836
SandroBeatz <amikhailov@intelliants.com>
2025-08-25 21:13:04 +0600

    feat: enhance MainLayout component with improved tab button styling and layout adjustments

--- frontend/src/app/ui/MainLayout/ui/MainLayout.vue
+++ frontend/src/app/ui/MainLayout/ui/MainLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {Settings, ListCheck} from "lucide-vue-next";
-import {IonLabel, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from "@ionic/vue";
+import {IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from "@ionic/vue";
 </script>
 
 <template>
@@ -9,13 +9,11 @@ import {IonLabel, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} fr
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/lists">
-          <ListCheck aria-hidden="true" />
-          <ion-label>Lists</ion-label>
+          <ListCheck aria-hidden="true" class="size-7" />
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/settings">
-          <Settings aria-hidden="true" />
-          <ion-label>Settings</ion-label>
+          <Settings aria-hidden="true" class="size-7" />
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -23,5 +21,9 @@ import {IonLabel, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} fr
 </template>
 
 <style scoped>
-
+ion-tab-bar {
+  margin: 16px;
+  border-radius: 16px;
+  height: 64px;
+}
 </style>
