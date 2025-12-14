6dce1e3e4bc6fdacc4919a498840fdcf4928bf94
SandroBeatz <amikhailov@intelliants.com>
2025-12-14 17:05:04 +0600

    feat: replace IconWiselistRounded with SVG logo and update styles for AuthPage

--- src/pages/Auth/ui/AuthPage.vue
+++ src/pages/Auth/ui/AuthPage.vue
@@ -3,7 +3,6 @@ import { IonButton } from '@ionic/vue'
 import { useRouter } from 'vue-router'
 import { useGoogleAuth } from '@/features/Auth'
 import { PageWrapper } from '@shared/ui/Page'
-import { IconWiselistRounded } from '@shared/ui/Icons'
 import { GoogleButton } from '@shared/ui/GoogleButton'
 import { AppleButton } from '@shared/ui/AppleButton'
 
@@ -15,10 +14,12 @@ const { googleAuth } = useGoogleAuth()
   <PageWrapper>
     <div class="flex-1 flex flex-col items-center justify-center text-center">
 
-      <IconWiselistRounded class="size-24 mb-6"/>
+      <div>
+        <img src="@app/assets/img/wiselist-icon.svg" alt="Wiselist Logo" class="h-36 w-36 object-contain" />
+      </div>
 
-      <h1 class="text-3xl font-semibold mb-2">Wiselist</h1>
-      <p class="text-lg text-zinc-500 px-10 mb-8">From shopping to travel — one list to rule them all.</p>
+      <h1 class="text-3xl uppercase font-bold mb-4">Wiselist</h1>
+      <p class="text-lg ion-text-mute px-10 mb-8">From shopping to travel — one list to rule them all.</p>
 
       <div class="flex flex-col gap-4 w-full">
         <AppleButton>Continue with Apple</AppleButton>
@@ -28,8 +29,8 @@ const { googleAuth } = useGoogleAuth()
     </div>
 
     <template #footer>
-      <p class="text-sm text-center text-zinc-500">
-        By continuing, you agree to our <a href="https://google.com" class="text-primary">Privacy Policy</a>.
+      <p class="text-sm text-center ion-text-mute">
+        By continuing, you agree to our <a href="https://google.com" class="ion-text-default">Privacy Policy</a>.
       </p>
     </template>
   </PageWrapper>
